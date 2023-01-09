70e6fc357cb41b193b4340f77e5467b6e1d6208d
Markus Luszczymak <m.luszczymak@avs-verkehrssicherheit.de>
2023-01-09 12:50:43 +0100

    Klasse Entries

---
+++ TestAPI/TestAPI/Models/Entries.cs
@@ -0,0 +1,15 @@
+﻿namespace TestAPI.Models
+{
+    public class Entries
+    {
+        public int Id { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public int UserId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
