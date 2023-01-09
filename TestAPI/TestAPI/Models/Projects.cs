3c663585719aeae7e88446f4391d21d15a374ced
Markus Luszczymak <m.luszczymak@avs-verkehrssicherheit.de>
2023-01-09 11:06:05 +0100

    Version 1.1 mit 2 Controllern

---
+++ TestAPI/TestAPI/Models/Projects.cs
@@ -0,0 +1,14 @@
+﻿namespace TestAPI.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace projects.Models
+{
+    public class Projects
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ TestAPI/TestAPI/Models/Users.cs
@@ -0,0 +1,6 @@
+﻿namespace TestAPI.Models
+{
+    public class Users
+    {
+    }
+}
